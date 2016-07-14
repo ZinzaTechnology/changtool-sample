b3914719ade1326bcf19840f2f5d4fe8a001b03b
cuongnx <cuong@zinza.com.vn>
2016-07-15 05:26:08 +0700

    fix error path in config/main.php

--- backend/config/main.php
+++ backend/config/main.php
@@ -48,7 +48,7 @@ return [
             ],
         ],
         'errorHandler' => [
-            'errorAction' => 'site/error',
+            'errorAction' => 'dashboard/error',
         ],
         'urlManager' => [
             'enablePrettyUrl' => true,
