a4469b8ba877ce71ac018324c8de13bb294c65fd
NguyenDucHoiZZ <dh01.hust@gmail.com>
2016-08-12 15:43:21 +1200

    sql change

--- sql/i_test_exam_insert_demo.sql
+++
@@ -1,6 +0,0 @@
-use chang_dev;
-INSERT INTO `test_exam` (`te_id`, `te_code`, `te_category`, `te_level`, `te_title`, `te_time`, `te_num_of_questions`, `te_created_at`, `te_last_updated_at`, `te_is_deleted`) VALUES
-(1, 'Php_Yii', 1, 1, 'Php Basic 2', 3000, 1, NULL, '2016-07-30 01:07:43', 0),
-(2, 'Php_Yii', 3, 1, 'Php Basic 1', 60, 50, NULL, NULL, 0),
-(3, 'C#_Yii', 2, 2, 'C# Basic', 50, 50, '2016-07-26 04:07:52', '2016-07-26 04:07:50', 0),
-(4, 'Yii ...', 2, 1, 'Php Advance', 200, 50, NULL, NULL, 0);
