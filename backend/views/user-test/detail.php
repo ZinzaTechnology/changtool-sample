374c07bb2588d29b71fb4a66b9f17a8a96178cd6
Ha inKi <vkhaiha@gmail.com>
2016-08-22 14:46:51 +0700

    Change showing

--- backend/views/user-test/detail.php
+++ backend/views/user-test/detail.php
@@ -3,14 +3,17 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\widgets\Breadcrumbs;
-
+use common\lib\components\AppConstant;
 /* @var $this yii\web\View */
 /* @var $model backend\models\TestExamQuestions */
 
 $this->title = $tile;
 $this->params['breadcrumbs'][] = ['label' => 'User Test', 'url' => Url::toRoute('/user-test')];
 $this->params['breadcrumbs'][] = $this->title;
-$question_count = ($page - 1) * $limitQuestion + 1;
+
+$this->registerJsFile('/res/js/plugins/dataTables/datatables.min.js', ['position' => static::POS_BEGIN]);
+$this->registerJsFile('/res/js/fr/userTest_do.js', ['position' => static::POS_BEGIN]);
+$this->registerCssFile('/res/css/plugins/dataTables/datatables.min.css');
 ?>
 
 <div class="ibox">
@@ -23,36 +26,45 @@ $question_count = ($page - 1) * $limitQuestion + 1;
         ?>
     </div>
     <div class="ibox-content">
-        <div class="OK"></div>
-        <div id="content">
-            <?php if (count($data)) : ?>
-                <?php foreach ($data as $question) : ?>
-                    <div class="element">
-                        <label class="control-label"><?= "Question {$question_count}" ?></label>
-                        <div class="alert alert-info">
-                            <strong><?= $question['qc_content'] ?></strong>
-                        </div>
-                        <div class="m-b-md">
-                            <?php foreach ($question['answer'] as $answer) : ?>
-                                <?php $checked = (isset($userAnswer["question-{$answer['qc_id']}"]) && in_array($answer['ac_id'], $userAnswer["question-{$answer['qc_id']}"])) ? true : false; ?>
-                                <div class="i-checks">
-                                    <?= Html::checkbox('answer', $checked, ['disabled' => '']) ?>
-                                    <?= $answer['ac_content'] ?>
-                                    <?= ($answer['ac_status'] == 1) ? "<span class='label label-primary'>True</span>" : '' ?>
-                                </div>
-                            <?php endforeach; ?>
-                        </div>
-                        <?php $question_count++; ?>
-                    </div>
+        <table class="table table-bordered" id="testTable">
+            <thead>
+            <th>No.</th>
+            <th>Content</th>
+            </thead>
+            <tbody>
+                <?php foreach (array_keys($userTestData->question_clones) as $idx => $qc_id) : ?>
+                    <?php $qu = $userTestData->question_clones[$qc_id]; ?>
+                    <tr>
+                        <td>Question <?= $idx + 1 ?></td>
+                        <td>
+                            <div class="alert alert-info"><?= $qu['qc_content'] ?></div>
+                            <?php if(isset($qu['answers'])): ?>
+                                <?php if ($qu['qc_type'] == AppConstant::QUESTION_TYPE_SINGLE_ANSWER) : ?>
+                                    <?php foreach ($qu['answers'] as $ans) : ?>
+                                        <?=
+                                        Html::radio("questions[$qc_id][]", false, [
+                                            'class' => 'i-checks',
+                                            'checked' => isset($userAnswer[$qc_id][$ans['ac_id']])? true : false,
+                                            'disabled' => true])
+                                        ?>
+                                        <?= Html::label($ans['ac_content'], "q_{$qc_id}_{$ans['ac_id']}") ?><br>
+                                    <?php endforeach; ?>
+                                <?php else : ?>
+                                    <?php foreach ($qu['answers'] as $ans) : ?>
+                                        <?=
+                                        Html::checkbox("questions[$qc_id][]", false, [
+                                            'class' => 'i-checks',
+                                            'checked' => isset($userAnswer[$qc_id][$ans['ac_id']])? true : false,
+                                            'disabled' => true])
+                                        ?>
+                                <?= Html::label($ans['ac_content'], "q_{$qc_id}_{$ans['ac_id']}") ?><br>
+                                    <?php endforeach; ?>
+                                <?php endif; ?>
+                            <?php endif; ?>
+                        </td>
+                    </tr>
                 <?php endforeach; ?>
-                <div class="form-group">
-                    <?= Html::a('Previous', ($page - 1 > 0) ? Url::current(['page' => $page - 1]) : '', ['class' => 'btn btn-default', 'disabled' => ($page - 1 > 0) ? false : true]) ?>
-                    <?= Html::a('Next', ($page < $pageMax) ? Url::current(['page' => $page + 1]) : '', ['class' => 'btn btn-default', 'disabled' => ($page < $pageMax) ? false : true]) ?>
-                </div>
-            <?php else : ?>
-                <strong>No question found</strong>"
-            <?php endif; ?>
-
-        </div>
+            </tbody>
+        </table>
     </div>
 </div>
