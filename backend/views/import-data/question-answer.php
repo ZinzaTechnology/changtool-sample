8383997d6a364629881158f8b964681b47939e8f
Ha inKi <vkhaiha@gmail.com>
2016-08-23 16:07:21 +0700

    Chang announce using setSessionFlash

--- backend/views/import-data/question-answer.php
+++ backend/views/import-data/question-answer.php
@@ -19,12 +19,6 @@ $this->params['breadcrumbs'][] = $this->title;
         ?>
     </div>
     <div class="ibox-content">
-        <?php if(!empty($success)): ?>
-            <div class="alert alert-success">
-                <?= $success ?>
-            </div>
-        <?php endif; ?>
-        
         <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]) ?>
         <?= $form->field($model, 'excelFile')->fileInput() ?>
         <?= Html::submitButton('Import', ['class' => 'btn btn-success']) ?>
