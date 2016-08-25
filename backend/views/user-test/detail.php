03b4bdeb5cf394f24863c42b5b81f2adf0f7de21
Ha inKi <vkhaiha@gmail.com>
2016-08-25 14:10:38 +0700

    Show questions content by html form

--- backend/views/user-test/detail.php
+++ backend/views/user-test/detail.php
@@ -13,6 +13,16 @@ $this->params['breadcrumbs'][] = $this->title;
 
 $this->registerJsFile('/res/js/plugins/dataTables/datatables.min.js', ['position' => static::POS_BEGIN]);
 $this->registerCssFile('/res/css/plugins/dataTables/datatables.min.css');
+
+$this->registerCssFile('/res/css/plugins/editormd.min.css');
+$this->registerJsFile('/res/lib/marked.min.js');
+$this->registerJsFile('/res/lib/prettify.min.js');
+$this->registerJsFile('/res/lib/flowchart.min.js');
+$this->registerJsFile('/res/lib/raphael.min.js');
+$this->registerJsFile('/res/lib/underscore.min.js');
+$this->registerJsFile('/res/lib/sequence-diagram.min.js');
+$this->registerJsFile('/res/lib/jquery.flowchart.min.js');
+$this->registerJsFile('/res/js/plugins/bootstrap-markdown/editormd.min.js');
 ?>
 <script type="text/javascript">
 $(document).ready(function(){
@@ -40,9 +50,10 @@ $(document).ready(function(){
                 <?php foreach (array_keys($userTestData->question_clones) as $idx => $qc_id) : ?>
                     <?php $qu = $userTestData->question_clones[$qc_id]; ?>
                     <tr>
-                        <td>Question <?= $idx + 1 ?></td>
+                        <td class="text-center">Question <?= $idx + 1 ?></td>
                         <td>
-                            <div class="alert alert-info"><?= $qu['qc_content'] ?></div>
+                            <div class="editormdCl" id="<?= $qu['qc_id']  ?>"></div>
+                            <div class="hidden" id="<?= $qu['qc_id'].'_hd' ?>"><?= json_encode($qu['qc_content']) ?></div>
                             <?php if(isset($qu['answers'])): ?>
                                 <?php if ($qu['qc_type'] == AppConstant::QUESTION_TYPE_SINGLE_ANSWER) : ?>
                                     <?php foreach ($qu['answers'] as $ans) : ?>
@@ -79,3 +90,28 @@ $(document).ready(function(){
         </table>
     </div>
 </div>
+
+<script>
+$(function() {
+    $(".editormdCl").each(function(idx, el) {
+        var el_id = el.id;
+        editormd.markdownToHTML(el.id, {
+            markdown        : JSON.parse($("#" + el_id + "_hd").html()),
+            //htmlDecode      : true,
+            htmlDecode      : "style,script,iframe",  // you can filter tags decode
+            //toc             : false,
+            tocm            : true,    // Using [TOCM]
+            //tocContainer    : "#custom-toc-container", 
+            //gfm             : false,
+            //tocDropdown     : true,
+            // markdownSourceCode : true, 
+            emoji           : true,
+            taskList        : true,
+            tex             : true,  
+            flowChart       : true,  
+            sequenceDiagram : true,  
+        });
+    });
+
+});
+</script>
