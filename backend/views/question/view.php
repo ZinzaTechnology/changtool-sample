78060a68f5497787ae9b5ddd1fe325fa761145ef
cuongnx <cuong@zinza.com.vn>
2016-08-25 10:04:19 +0700

    show html preview of question in question/index

--- backend/views/question/view.php
+++ backend/views/question/view.php
@@ -3,113 +3,88 @@ use yii\helpers\Html;
 use yii\widgets\Breadcrumbs;
 use yii\helpers\Url;
 use yii\grid\GridView;
+
 $this->registerJsFile('/res/js/plugins/bootstrap-markdown/editormd.min.js');
 $this->registerCssFile('/res/css/plugins/editormd.min.css');
-//$this->registerJsFile('/res/lib/marked.min.js');
-//$this->registerJsFile('/res/lib/prettify.min.js');
-//$this->registerJsFile('/res/lib/flowchart.min.js');
-//$this->registerJsFile('/res/lib/raphael.min.js');
-//$this->registerJsFile('/res/lib/underscore.min.js');
-//$this->registerJsFile('/res/lib/sequence-diagram.min.js');
-//$this->registerJsFile('/res/lib/jquery.flowchart.min.js');
 
 $this->title = 'Question Manager';
-$this->params ['breadcrumbs'] [] = [
+$this->params['breadcrumbs'][] = [
     'label' => 'Question Manager',
     'url' => [
-        'index' 
-    ] 
+        'index'
+    ]
 ];
-($question) ? $this->params ['breadcrumbs'] [] = $question->q_id : null;
+($question) ? $this->params['breadcrumbs'][] = $question->q_id : null;
 ?>
 
 <div class="ibox">
-	<div class="ibox-title">
-		<h1><?= $this->title ?></h1>
+    <div class="ibox-title">
+        <h1><?= $this->title ?></h1>
         <?=Breadcrumbs::widget([ 'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [ ] ])?>
     </div>
-	<br>
-	<div class="ibox-content">	
-	
+    <br>
+    <div class="ibox-content">  
+    
         <?php if ($question) : ?>
             <table class="table table-striped">
-			<tr>
-				<td colspan="2"><h4>Question content </h2><br>
-					<div id="editormd-view">
-						<textarea class="editormd-markdown-textarea" name="Question[q_content]"></textarea>
-        				<textarea class="editormd-html-textarea" name="Question[q_content_html]"></textarea>
-					</div>
-				</td>
-				
-				<script>
-	            $(function() {
-	            	var editor = editormd("editormd-view", {
-	               	 width  : "100%",
-	               	 readOnly: true,
-	               	 markdown: <?= json_encode($question['q_content'])?>,
-	                 path : "/res/lib/", // Autoload modules mode, codemirror, marked... dependents libs path
-	               });
-// 	                var testEditormdView;
-// 				    testEditormdView = editormd.markdownToHTML("test-editormd-view", {
-//                        markdown        : <?= json_encode($question->q_content) ?>,//+ "\r\n" + $("#append-test").text(),
-//                         //htmlDecode      : true,      
-//                         htmlDecode      : "style,script,iframe",  // you can filter tags decode
-//                         //toc             : false,
-//                         tocm            : true,    // Using [TOCM]
-//                         //tocContainer    : "#custom-toc-container", 
-//                         //gfm             : false,
-//                         //tocDropdown     : true,
-//                         // markdownSourceCode : true, 
-//                         emoji           : true,
-//                         taskList        : true,
-//                         tex             : true,  
-//                         flowChart       : true,  
-//                         sequenceDiagram : true,  
-//                     });
-               
-	            });
-				</script>
-				
-				
-			</tr>
-			<tr>
-				<td>Category</td>
-				<td><?= $category[$question->q_category]?></td>
-			</tr>
-			<tr>
-				<td>Type</td>
-				<td><?= $type[$question->q_type] ?></td>
-			</tr>
-			<tr>
-				<td>Created date</td>
-				<td><?= $question->created_at ?></td>
-			</tr>
-			<tr>
-				<td>Updated date</td>
-				<td><?= $question->updated_at ?></td>
-			</tr>
-			
-		</table>
+            <tr>
+                <td colspan="2"><h4>Question content </h2><br>
+                    <div id="editormd-view">
+                        <textarea class="editormd-markdown-textarea" name="Question[q_content]"></textarea>
+                        <textarea class="editormd-html-textarea" name="Question[q_content_html]"></textarea>
+                    </div>
+                </td>
+                
+                <script>
+                $(function() {
+                    var editor = editormd("editormd-view", {
+                     width  : "100%",
+                     readOnly: true,
+                     markdown: <?= json_encode($question['q_content'])?>,
+                     path : "/res/lib/", // Autoload modules mode, codemirror, marked... dependents libs path
+                   });
+                });
+                </script>
+                
+            </tr>
+            <tr>
+                <td>Category</td>
+                <td><?= $category[$question->q_category]?></td>
+            </tr>
+            <tr>
+                <td>Type</td>
+                <td><?= $type[$question->q_type] ?></td>
+            </tr>
+            <tr>
+                <td>Created date</td>
+                <td><?= $question->created_at ?></td>
+            </tr>
+            <tr>
+                <td>Updated date</td>
+                <td><?= $question->updated_at ?></td>
+            </tr>
+            
+        </table>
 
-	</div>
-	<br>
-	<div class="ibox-content">
-		<h3>Answer</h3>
-		
-                 <?php
+    </div>
+    <br>
+    <div class="ibox-content">
+        <h3>Answer</h3>
+        
+            <?php
             echo GridView::widget([
                 'dataProvider' => $dataProvider,
                 'columns' => [
                     [
-                        'class' => 'yii\grid\SerialColumn' 
+                        'class' => 'yii\grid\SerialColumn'
                     ],
                     'qa_id',
                     'qa_content',
                     [
                         'attribute' => 'category',
                         "content" => function ($model, $key, $index, $column) use ($answer_status) {
-                            return $answer_status [$model->qa_status];
-                        } 
+                            return $answer_status[$model->qa_status];
+                        }
                     ],
                     'created_at',
                     'updated_at',
@@ -125,9 +100,9 @@ $this->params ['breadcrumbs'] [] = [
                             'delete' => function ($url, $model) {
                                 return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, [
                                     'data-confirm' => 'Are you sure you want to delete this answer?',
-                                    'data-method' => 'post' 
+                                    'data-method' => 'post'
                                 ]);
-                            } 
+                            }
                         ],
                         
                         'urlCreator' => function ($action, $dataProvider, $key, $index) {
@@ -135,32 +110,32 @@ $this->params ['breadcrumbs'] [] = [
                             if ($action === 'edit') {
                                 return Url::to([
                                     'question/edit-answer',
-                                    'q_id' => $dataProvider ['q_id'],
-                                    'qa_id' => $dataProvider ['qa_id'] 
+                                    'q_id' => $dataProvider['q_id'],
+                                    'qa_id' => $dataProvider['qa_id']
                                 ]);
                             }
                             if ($action === 'delete') {
                                 return Url::to([
                                     'question/delete-answer',
-                                    'q_id' => $dataProvider ['q_id'],
-                                    'qa_id' => $dataProvider ['qa_id'] 
+                                    'q_id' => $dataProvider['q_id'],
+                                    'qa_id' => $dataProvider['qa_id']
                                 ]);
                             }
                             return $url;
-                        } 
-                    ] 
-                ] 
+                        }
+                    ]
+                ]
             ]);
             ?>  
- 		<?php else : ?>
+        <?php else : ?>
             Question not found
         <?php endif; ?>
         </div>
 
-	<br>
-	<div class="ibox-content">
+    <br>
+    <div class="ibox-content">
         <?= Html::a('Edit', ['/question/edit-question', 'q_id' => $question->q_id], ['class' => 'btn btn-success'])?>
         <?=Html::a('Delete', ['/question/delete','q_id' => $question->q_id ], ['class' => 'btn btn-danger','data' => ['confirm' => 'Are you sure you want to delete this item?','method' => 'post' ] ])?>
         <?= Html::a('Back', ['/question/index'], ['class' => 'btn btn-primary pull-right'])?>
-   		</div>
+        </div>
 </div>
