e9587d8a70289aab76001ab2758e054899470d5f
cuongnx <cuong@zinza.com.vn>
2016-08-26 17:29:29 +0700

    remove unused sql

--- sql/0001_alter_user.sql
+++
@@ -1,8 +0,0 @@
-alter table user change column u_phone u_phone varchar(255);
-alter table user change column u_name u_name varchar(32) not null  unique;
-alter table user add column u_fullname varchar(255) not null;
-alter table user change column u_mail u_mail varchar(255);
-alter table user change cloumn u_created_at created_at datetime;
-alter table user change cloumn u_updated_at updated_at datetime;
-alter table user change column u_is_deleted is_deleted int(4) default 0;
-alter table question_clone add column q_id int(11) not null;
