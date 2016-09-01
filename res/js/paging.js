c741968bc2ca1361ffe11bc3ef5d258a3d64f737
Nguyen Trung Tho <trungtho6789@gmail.com>
2016-09-01 04:54:28 -0400

    Fixbug: overload paging number.

--- res/js/paging.js
+++ res/js/paging.js
@@ -4,6 +4,7 @@ function Pager(tableName, itemsPerPage) {
     this.currentPage = 1;
     this.pages = 0;
     this.inited = false;
+    this.range = 9; //Number of button you want to display
     
     this.showRecords = function(from, to) {        
         var rows = document.getElementById(tableName).rows;
@@ -21,16 +22,19 @@ function Pager(tableName, itemsPerPage) {
     		alert("not inited");
     		return;
     	}
-
+        
         var oldPageAnchor = document.getElementById('pg'+this.currentPage);
         oldPageAnchor.className = 'pg-normal';
         
+        // Calculte range of page to display
+        pager.showPageNav('pager', 'pageNavPosition', pageNumber);
+
         this.currentPage = pageNumber;
         var newPageAnchor = document.getElementById('pg'+this.currentPage);
         newPageAnchor.className = 'pg-selected';
         
         var newPageAnchorPrev = document.getElementById('pg_prev');
-        if(this.currentPage == 1) {
+        if (this.currentPage == 1) {
             newPageAnchorPrev.className = 'pg-selected';
         } else {
             newPageAnchorPrev.className = 'pg-normal';
@@ -66,15 +70,33 @@ function Pager(tableName, itemsPerPage) {
         this.inited = true;
     };
 
-    this.showPageNav = function(pagerName, positionId) {
+    this.showPageNav = function(pagerName, positionId, currentPage) {
     	if (! this.inited) {
     		alert("not inited");
     		return;
     	}
+
+        var pageMin = 1;
+        var pageMax = this.pages;
+        var middle = Math.floor(this.range / 2);
+        if ((currentPage - middle) <= 0) {
+            pageMin = 1;
+            pageMax = ((this.range - this.pages) < 0) ? this.range : this.pages;
+        } else {
+            if ((currentPage + middle) > this.pages) {
+                pageMax = this.pages;
+                pageMin = ((this.pages - this.range) > 0) ? ((this.pages - this.range) + 1) : 1;
+            } else {
+                var added_pageMin = (this.range % 2 === 1) ? 0 : 1;
+                pageMax = currentPage + middle;
+                pageMin = (currentPage - middle) + added_pageMin;
+            }
+        }
+        
     	var element = document.getElementById(positionId);
     	var pagerHtml = '<div class=paging_client>';
         pagerHtml += '<span id="pg_prev" onclick="' + pagerName + '.prev();" class="pg-normal"> << </span>';
-        for (var page = 1; page <= this.pages; page++) {
+        for (var page = pageMin; page <= pageMax; page++) {
             pagerHtml += '<span id="pg' + page + '" class="pg-normal" onclick="' + pagerName + '.showPage(' + page + ');">' + page + '</span>';
         }
         pagerHtml += '<span id="pg_next" onclick="'+pagerName+'.next();" class="pg-normal"> >> </span>';  
